b7a93dff76731067afea69d104912aa4bcff9760
0xAutomatee <jcovti680@gmail.com>
2026-04-11 01:27:40 +0500

    Update sql important commands.sql

--- sql important commands.sql
+++ sql important commands.sql
@@ -8,3 +8,24 @@ WHERE TABLE_TYPE = 'BASE TABLE'
 -- Back up the overstockDB database to a specified location
 BACKUP DATABASE overstockDB
 TO DISK = 'D:\overstockDB.bak';
+
+
+-- Task: Remove duplicate rows from rpt_02a_cr_sheet table while ensuring data integrity.
+BEGIN TRAN; --begin transaction to ensure data integrity  
+
+
+-- Step 1: Keep only distinct rows in temp table
+SELECT DISTINCT *
+FROM rpt_02a_cr_sheet;
+
+-- Step 2: Empty original table
+TRUNCATE TABLE rpt_02a_cr_sheet;
+
+-- Step 3: Insert clean data back
+INSERT INTO rpt_02a_cr_sheet
+SELECT * FROM #temp_clean;
+
+-- Step 4: Drop temp table
+DROP TABLE #temp_clean;
+
+COMMIT;
